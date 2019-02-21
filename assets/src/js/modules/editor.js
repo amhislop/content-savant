13a991f407c744b23a79b63f369b72ea78e4aa78
Aidan Hislop <31024174+amhislop@users.noreply.github.com>
2019-02-22 10:32:37 +1100

    Update to SCSS

--- assets/src/js/modules/editor.js
+++ assets/src/js/modules/editor.js
@@ -1,16 +1,15 @@
-export const getEditorSettings = ({theme, mode: {javascript}}) => {
-    var editorSettings = wp.codeEditor.defaultSettings ? _.clone( wp.codeEditor.defaultSettings ) : {};
-    console.log('initialise');
-    editorSettings.codemirror = _.extend(
-        {},
-        editorSettings.codemirror,
-        {
-            indentUnit: 2,
-            tabSize: 2,
-            theme,
-            mode: javascript,
-        }
-    );
+export const getEditorSettings = ({ theme, mode: { javascript } }) => {
+  console.log(wp.codeEditor);
+  var editorSettings = wp.codeEditor.defaultSettings
+    ? _.clone(wp.codeEditor.defaultSettings)
+    : {};
+  console.log('initialise');
+  editorSettings.codemirror = _.extend({}, editorSettings.codemirror, {
+    indentUnit: 2,
+    tabSize: 2,
+    theme,
+    mode: javascript
+  });
 
-    return editorSettings;
-}
+  return editorSettings;
+};
