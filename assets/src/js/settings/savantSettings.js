60f77fb8e9190fadc9378dd6c56bf4285cd06d0e
Aidan Hislop <31024174+amhislop@users.noreply.github.com>
2019-02-22 15:48:07 +1100

    Update 2

--- assets/src/js/settings/savantSettings.js
+++ assets/src/js/settings/savantSettings.js
@@ -1,3 +1,30 @@
-export default function savantSettings() {
-  return <div>Some content is rendering</div>;
+export default class savantSettings extends wp.element.Component {
+  state = {
+    name: 'Aidan'
+  };
+
+  render() {
+    return <div>Hello {this.state.name}</div>;
+  }
 }
+
+// class App extends wp.element.Component {
+//   onClick() {
+//     console.log('click');
+//     console.log(this.state);
+//   }
+
+//   render() {
+//     return (
+//       <Provider>
+//         <Menu />
+//         <div>
+//           <textarea id="savantEditor" value={'code'} />
+//         </div>
+//         <Data />
+//       </Provider>
+//     );
+//   }
+// }
+
+// export default App;
